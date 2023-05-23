45c7f786307ba8b955afad41fa99e0eed90cdce3
serhii.ku <serhii.ku@voicenter.com>
2023-05-23 14:04:59 +0300

    update: added build for demo

---
+++ docs/src/.vuepress/components/DemoView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+
+    <!--    <div v-html="htmlContent"></div>-->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      htmlContent: ''
+    };
+  },
+  mounted() {
+    // Load the HTML file or fetch it from an API
+
+    //this.loadHTMLFile();
+  },
+  methods: {
+    /*loadHTMLFile() {
+      // Simulated example - you can replace this with your own logic to load the HTML content
+      const htmlFile = 'https://github.com/VoicenterTeam/opensips-js/blob/master/demo/index.html';
+
+      // Fetch the HTML content
+      fetch(htmlFile)
+          .then(response => response.text())
+          .then(data => {
+            this.htmlContent = data;
+          })
+          .catch(error => {
+            console.error('Error loading HTML file:', error);
+          });
+    }*/
+  }
+};
+</script>
