997a30b4ad0790618dc99bb144136843df7b535f
Bohdan Konechnyi <bohdan.konechniy@gmail.com>
2023-12-04 11:44:47 +0100

    Update: eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,14 @@
 /* eslint-env node */
 module.exports = {
     root: true,
+    parser: '@typescript-eslint/parser',
     extends: [
-        'plugin:@typescript-eslint/recommended',
-        'eslint:recommended'
+        '@voicenter-team/ts'
     ],
     ignorePatterns: [
         'src/helpers/webrtcmetrics/',
     ],
-    rules: {
-        '@typescript-eslint/ban-ts-comment': [ 'error' ],
-        '@typescript-eslint/no-unused-vars': 'error',
-        'no-unused-vars': 'off',
-        'no-console': [ 'error', { allow: [ 'warn', 'error' ] } ],
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'semi': [ 'error', 'never' ],
-        'quotes': [ 'error', 'single' ],
-        'object-curly-spacing': [ 'error', 'always' ],
-        'array-bracket-spacing': [ 'error', 'always' ],
-        'space-before-function-paren': [ 'error', 'always' ],
-        '@typescript-eslint/no-var-requires': 0,
-        'indent': [ 'error', 4, { 'SwitchCase': 1 } ],
-    },
+    rules: {},
     env: {
         es2021: true,
         browser: true,
